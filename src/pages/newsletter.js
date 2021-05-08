dcc1ef1dc334db7f8caf34f9a88946923c8b2ff8
webdevelopua <js.develop.com@gmail.com>
2021-05-08 23:26:52 +0300

    Add Newsletter page

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -2,7 +2,35 @@ import React from 'react'
 import Layout from '../components/Layout'
 
 const NewsLetter = () => {
-  return <h2>newsletter page</h2>
+  return (
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Just subscribe!</h2>
+          <h4>enter you name & email</h4>
+          <form className="contact-form">
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="form-control"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              className="form-control"
+              required
+            />
+            <button type="submit" className="btn form-control submit-btn">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
+  )
 }
 
 export default NewsLetter
