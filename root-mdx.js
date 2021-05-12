adf3434562311aab8627fc41e83f2127542b6b15
webdevelopua <js.develop.com@gmail.com>
2021-05-12 22:09:38 +0300

    Add inlineCode custom styling

--- root-mdx.js
+++ root-mdx.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import { Headings } from './src/components/PostComponents'
+import { Code, Headings } from './src/components/PostComponents'
 
 const components = {
   h2: Headings.myH2,
   h4: Headings.myH4,
+  inlineCode: Code,
 }
 
 export const wrapMDX = ({ element }) => (
